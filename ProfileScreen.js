57735ffdf24c3e97c0b82f92f859bac05261a7be
khamngern <moupig2@gmail.com>
2024-05-30 09:13:02 +0700

    first tab

---
+++ ProfileScreen.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+
+const ProfileScreen = () => {
+
+return (
+
+<View
+
+style={{
+
+flex: 1,
+
+alignItems: "center",
+
+justifyContent: "center",
+
+backgroundColor: "pink",
+
+}}
+
+>
+
+<Text>Profile Screen</Text>
+
+</View>
+
+);
+
+};
+
+export default ProfileScreen;
